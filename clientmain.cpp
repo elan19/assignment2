8119918b0385647b66c9e8ea6339db3cf4aed495
elan19 <elan19@student.bth.se>
2021-02-17 17:07:02 +0100

    skickar till server, men får inte recvfrom att fungera korrekt

--- clientmain.cpp
+++ clientmain.cpp
@@ -31,6 +31,9 @@ int main(int argc, char *argv[]){
   sa.ai_socktype = SOCK_DGRAM;
   sa.ai_protocol = 17;
 
+  calcMessage msg;
+  calcProtocol protMsg;
+
   if (int rv = getaddrinfo(Desthost, Destport, &sa, &si) != 0)
   {
     fprintf(stderr, "%s\n", gai_strerror(rv));
@@ -54,23 +57,19 @@ int main(int argc, char *argv[]){
   if (p == NULL)
   {
     printf("NULL\n");
+    close(sockfd);
     exit(0);
   }
 
-  printf("hejsan\n");
-
-  memset(&servaddr, 0, sizeof(servaddr));
-
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(port);
-  servaddr.sin_addr.s_addr = INADDR_ANY;
-
-  int n, len;
-  char buffer[256];
-
-  if(sendto(sockfd, 0, 0, NULL, p->ai_addr, p->ai_addrlen) == -1)
+  ssize_t sentbytes;
+  msg.type = 22;
+  msg.message = 0;
+  msg.protocol = 17;
+  msg.major_version = 1;
+  msg.minor_version = 0;
+  if((sentbytes = sendto(sockfd, &msg, sizeof(msg),0,p->ai_addr, p->ai_addrlen)) == -1)
   {
-    printf("%s\n", strerror(errno));
+    printf("Error: Couldnt send to the server.");
     close(sockfd);
     exit(0);
   }
@@ -78,15 +77,54 @@ int main(int argc, char *argv[]){
   freeaddrinfo(si);
 
   socklen_t addr_len = sizeof(servaddr);
-  if(n = recvfrom(sockfd, buffer, 256,0, (struct sockaddr *)&servaddr, &addr_len) == -1)
+  if(recvfrom(sockfd, &protMsg, sizeof(protMsg),0, (struct sockaddr *)&servaddr, &addr_len) == -1)
   {
     printf("Error:Couldnt recieve from server.\n");
     close(sockfd);
     exit(0);
   }
 
+  if(protMsg.arith == 1)
+  {
+    protMsg.inResult = protMsg.inValue1 + protMsg.inValue2;
+    printf("Add %d %d\n", protMsg.inValue1, protMsg.inValue2);
+  }
+  else if(protMsg.arith == 2)
+  {
+    protMsg.inResult = protMsg.inValue1 - protMsg.inValue2;
+    printf("Sub %d %d\n", protMsg.inValue1, protMsg.inValue2);
+  }
+  else if(protMsg.arith == 3)
+  {
+    protMsg.inResult = protMsg.inValue1 * protMsg.inValue2;
+    printf("Mul %d %d\n", protMsg.inValue1, protMsg.inValue2);
+  }
+  else if(protMsg.arith == 4)
+  {
+    protMsg.inResult = protMsg.inValue1 / protMsg.inValue2;
+    printf("Div %d %d\n", protMsg.inValue1, protMsg.inValue2);
+  }
+  else if(protMsg.arith == 5)
+  {
+    protMsg.flResult = protMsg.flValue1 + protMsg.flValue2;
+    printf("fAdd %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+  }
+  else if(protMsg.arith == 6)
+  {
+    protMsg.flResult = protMsg.flValue1 - protMsg.flValue2;
+    printf("fSub %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+  }
+  else if(protMsg.arith == 7)
+  {
+    protMsg.flResult = protMsg.flValue1 * protMsg.flValue2;
+    printf("fMul %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+  }
+  else if(protMsg.arith == 8)
+  {
+    protMsg.flResult = protMsg.flValue1 / protMsg.flValue2;
+    printf("fDiv %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+  }
 
-  printf("%s\n", buffer);
 
   printf("Test\n");
 
