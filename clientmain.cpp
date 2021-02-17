b05a5b34779681ba0032b90de8dc85cc2297ecd8
elan19 <elan19@student.bth.se>
2021-02-17 19:22:27 +0100

    Recievar och skickar till servern på rätt sätt, debugg och timeout kvar

--- clientmain.cpp
+++ clientmain.cpp
@@ -32,6 +32,7 @@ int main(int argc, char *argv[]){
   sa.ai_protocol = 17;
 
   calcMessage msg;
+  calcMessage *message = new calcMessage{};
   calcProtocol protMsg;
 
   if (int rv = getaddrinfo(Desthost, Destport, &sa, &si) != 0)
@@ -62,11 +63,11 @@ int main(int argc, char *argv[]){
   }
 
   ssize_t sentbytes;
-  msg.type = 22;
-  msg.message = 0;
-  msg.protocol = 17;
-  msg.major_version = 1;
-  msg.minor_version = 0;
+  msg.type = htons(22);
+  msg.message = htonl(0);
+  msg.protocol = htons(17);
+  msg.major_version = htons(1);
+  msg.minor_version = htons(0);
   if((sentbytes = sendto(sockfd, &msg, sizeof(msg),0,p->ai_addr, p->ai_addrlen)) == -1)
   {
     printf("Error: Couldnt send to the server.");
@@ -74,60 +75,123 @@ int main(int argc, char *argv[]){
     exit(0);
   }
 
+  printf("Sent message to server.\n");
   freeaddrinfo(si);
 
   socklen_t addr_len = sizeof(servaddr);
-  if(recvfrom(sockfd, &protMsg, sizeof(protMsg),0, (struct sockaddr *)&servaddr, &addr_len) == -1)
+  int bytes = recvfrom(sockfd, &protMsg, sizeof(protMsg),0, (struct sockaddr *)&servaddr, &addr_len);
+  if(bytes == -1)
   {
     printf("Error:Couldnt recieve from server.\n");
     close(sockfd);
     exit(0);
   }
 
+  if(sizeof(calcMessage) == bytes)
+  {
+    delete message;
+    message = (calcMessage*)&protMsg;
+    message->message = ntohl(message->message);
+    message->type = ntohs(message->type);
+    message->major_version = ntohs(message->major_version);
+    message->minor_version = ntohs(message->minor_version);
+
+    if(message->type == 2 && message->message == 2 && message->major_version == 1 && 
+    message->minor_version == 0)
+    {
+      printf("Wrong type recieved. Expected a calcProtocol!\n");
+      close(sockfd);
+      exit(0);
+    }
+    printf("Error: \n");
+
+
+  }
+  else
+  {
+    protMsg.arith = ntohl(protMsg.arith);
+    protMsg.inValue1 = ntohl(protMsg.inValue1);
+    protMsg.inValue2 = ntohl(protMsg.inValue2);
+    protMsg.inResult = ntohl(protMsg.inResult);
+    protMsg.id = ntohl(protMsg.id);
+    protMsg.major_version = ntohs(protMsg.major_version);
+    protMsg.minor_version = ntohs(protMsg.minor_version);
+    protMsg.type = ntohs(protMsg.type);
+
   if(protMsg.arith == 1)
   {
     protMsg.inResult = protMsg.inValue1 + protMsg.inValue2;
-    printf("Add %d %d\n", protMsg.inValue1, protMsg.inValue2);
+    printf("Add %d %d, result: %d\n", protMsg.inValue1, protMsg.inValue2, protMsg.inResult);
   }
   else if(protMsg.arith == 2)
   {
     protMsg.inResult = protMsg.inValue1 - protMsg.inValue2;
-    printf("Sub %d %d\n", protMsg.inValue1, protMsg.inValue2);
+    printf("Sub %d %d, result: %d\n", protMsg.inValue1, protMsg.inValue2, protMsg.inResult);
   }
   else if(protMsg.arith == 3)
   {
     protMsg.inResult = protMsg.inValue1 * protMsg.inValue2;
-    printf("Mul %d %d\n", protMsg.inValue1, protMsg.inValue2);
+    printf("Mul %d %d, result: %d\n", protMsg.inValue1, protMsg.inValue2, protMsg.inResult);
   }
   else if(protMsg.arith == 4)
   {
     protMsg.inResult = protMsg.inValue1 / protMsg.inValue2;
-    printf("Div %d %d\n", protMsg.inValue1, protMsg.inValue2);
+    printf("Div %d %d, result: %d\n", protMsg.inValue1, protMsg.inValue2, protMsg.inResult);
   }
   else if(protMsg.arith == 5)
   {
     protMsg.flResult = protMsg.flValue1 + protMsg.flValue2;
-    printf("fAdd %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+    printf("fAdd %lf %lf, result: %lf\n", protMsg.flValue1, protMsg.flValue2, protMsg.flResult);
   }
   else if(protMsg.arith == 6)
   {
     protMsg.flResult = protMsg.flValue1 - protMsg.flValue2;
-    printf("fSub %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+    printf("fSub %lf %lf, result: %lf\n", protMsg.flValue1, protMsg.flValue2, protMsg.flResult);
   }
   else if(protMsg.arith == 7)
   {
     protMsg.flResult = protMsg.flValue1 * protMsg.flValue2;
-    printf("fMul %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+    printf("fMul %lf %lf, result: %lf\n", protMsg.flValue1, protMsg.flValue2, protMsg.flResult);
   }
   else if(protMsg.arith == 8)
   {
     protMsg.flResult = protMsg.flValue1 / protMsg.flValue2;
-    printf("fDiv %lf %lf\n", protMsg.flValue1, protMsg.flValue2);
+    printf("fDiv %lf %lf, result: %lf\n", protMsg.flValue1, protMsg.flValue2, protMsg.flResult);
   }
 
+    protMsg.arith = htonl(protMsg.arith);
+    protMsg.inValue1 = htonl(protMsg.inValue1);
+    protMsg.inValue2 = htonl(protMsg.inValue2);
+    protMsg.inResult = htonl(protMsg.inResult);
+    protMsg.id = htonl(protMsg.id);
+    protMsg.major_version = htons(protMsg.major_version);
+    protMsg.minor_version = htons(protMsg.minor_version);
+    protMsg.type = htons(protMsg.type);
 
-  printf("Test\n");
+    if((sentbytes = sendto(sockfd, &protMsg, sizeof(protMsg),0,p->ai_addr, p->ai_addrlen)) == -1)
+  {
+    printf("Error: Couldnt send to the server.");
+    close(sockfd);
+    exit(0);
+  }
+
+  bytes = recvfrom(sockfd, message, sizeof(*message),0, (struct sockaddr *)&servaddr, &addr_len);
+  message->message = ntohl(message->message);
+
+  if(message->message == 1)
+  {
+    printf("OK!\n");
+  }
+  else
+  {
+    printf("NOT OK!\n");
+  }
+
+  printf("%d\n", message->message);
+
+}
 
   close(sockfd);
+  delete message;
   return 0;
 }
