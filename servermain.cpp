6b0d58a6e04dd337cf7e1f01c08727cf29c1a481
elan19 <elan19@student.bth.se>
2021-02-23 17:39:56 +0100

    Påbörjat servern, kan ta emot protocol fast inte på korrekt sett

--- servermain.cpp
+++ servermain.cpp
@@ -3,68 +3,155 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
 
 /* You will to add includes here */
 
-
 // Included to get the support library
-#include <calcLib.h>
+#include "calcLib.h"
 
 #include "protocol.h"
 
-
 using namespace std;
 /* Needs to be global, to be rechable by callback and main */
-int loopCount=0;
-int terminate=0;
-
+int loopCount = 0;
+int terminate = 0;
 
 /* Call back function, will be called when the SIGALRM is raised when the timer expires. */
-void checkJobbList(int signum){
+void checkJobbList(int signum)
+{
   // As anybody can call the handler, its good coding to check the signal number that called it.
 
   printf("Let me be, I want to sleep.\n");
 
-  if(loopCount>20){
+  if (loopCount > 20)
+  {
     printf("I had enough.\n");
-    terminate=1;
+    terminate = 1;
   }
-  
+
   return;
 }
 
+int main(int argc, char *argv[])
+{
 
-
-
-
-int main(int argc, char *argv[]){
-  
   /* Do more magic */
 
+  if (argc != 2)
+  {
+    printf("Wrong format IP:PORT\n");
+    exit(0);
+  }
+
+  /* Do magic */
+
+  char delim[] = ":";
+  char *Desthost = strtok(argv[1], delim);
+  char *Destport = strtok(NULL, delim);
+
+  if (Desthost == NULL || Destport == NULL)
+  {
+    printf("Wrong format.\n");
+    exit(0);
+  }
+
+  int port = atoi(Destport);
+  int sockfd, connfd, len;
+  struct sockaddr_in cli;
+  addrinfo sa, *si, *p;
+  sa.ai_family = AF_INET;
+  sa.ai_socktype = SOCK_DGRAM;
+  sa.ai_protocol = 17;
+
+  calcMessage msg;
+  calcProtocol protMsg;
+
+  if (int rv = getaddrinfo(Desthost, Destport, &sa, &si) != 0)
+  {
+    fprintf(stderr, "%s\n", gai_strerror(rv));
+    exit(0);
+  }
+
+  struct sockaddr_in servaddr;
+
+  for (p = si; p != NULL; p = p->ai_next)
+  {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      printf("Error: Failed to create socket.\n");
+      continue;
+    }
+
+    if ((bind(sockfd, p->ai_addr, p->ai_addrlen)) != 0)
+    {
+      printf("Error: Couldnt bind!\n");
+      close(sockfd);
+      continue;
+    }
+    break;
+  }
+
+  if (p == NULL)
+  {
+    printf("NULL\n");
+    freeaddrinfo(si);
+    close(sockfd);
+    exit(0);
+  }
+
+  struct timeval tv;
+  tv.tv_sec = 2;
+  tv.tv_usec = 0;
+  ssize_t sentbytes;
+  int tries = 0;
+  int bytes = 0;
+  socklen_t addr_len = sizeof(servaddr);
+
+  setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+
+  len = sizeof(cli);
+  bool clientIsActive = false;
+  char buffer[128];
+  char recvBuffer[128];
 
   /* 
      Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
   */
   struct itimerval alarmTime;
-  alarmTime.it_interval.tv_sec=10;
-  alarmTime.it_interval.tv_usec=10;
-  alarmTime.it_value.tv_sec=10;
-  alarmTime.it_value.tv_usec=10;
+  alarmTime.it_interval.tv_sec = 10;
+  alarmTime.it_interval.tv_usec = 10;
+  alarmTime.it_value.tv_sec = 10;
+  alarmTime.it_value.tv_usec = 10;
 
   /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
   signal(SIGALRM, checkJobbList);
-  setitimer(ITIMER_REAL,&alarmTime,NULL); // Start/register the alarm. 
+  setitimer(ITIMER_REAL, &alarmTime, NULL); // Start/register the alarm.
 
-  
-  while(terminate==0){
-    printf("This is the main loop, %d time.\n",loopCount);
+  while (terminate == 0)
+  {
+    printf("This is the main loop, %d time.\n", loopCount);
+    if (recvfrom(sockfd, &msg, sizeof(msg), 0, (struct sockaddr *)&servaddr, &addr_len) == -1)
+    {
+      printf("Error: Recieve timeout, sending error to client!\n");
+      send(connfd, "ERROR TO\n", strlen("ERROR TO\n"), 0);
+      close(connfd);
+    }
+
+    msg.protocol = htons(msg.protocol);
+
+    printf("%d\n", msg.protocol);
     sleep(1);
     loopCount++;
   }
 
   printf("done.\n");
-  return(0);
-
-
-  
+  return (0);
 }
